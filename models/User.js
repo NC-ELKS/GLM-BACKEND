23fe58b29a7331ff18b6c2876b59938da4d312e0
liamcf44 <liamcf44@protonmail.com>
2018-06-25 16:40:42 +0100

    Initial commit on userTable branch, made model for key attributes of users

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const userTable = {
+  TableName: 'Users',
+  KeySchema: [
+    {
+      AttributeName: 'username',
+      KeyType: 'HASH'
+    }
+  ],
+  AttributeDefinitions: [
+    {
+      AttributeName: 'username',
+      AttributeType: 'S'
+    }
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 1,
+    WriteCapacityUnits: 1
+  }
+};
+
+module.exports = { userTable };
