72f02ab0e9ed41a4725082d5d1fce63c820464f6
liamcf44 <liamcf44@protonmail.com>
2018-06-27 16:08:29 +0100

    Added ability to get users by username

--- handler.js
+++ handler.js
@@ -3,6 +3,7 @@
 const addMessage = require('./handlers/addMessage');
 const getMessagesByUsername = require('./handlers/getMessagesByUsername');
 const viewAllMessages = require('./handlers/viewAllMessages');
+const getUserDetails = require('./handlers/getUserDetails');
 
 const createMessage = (event, context, cb) => {
   const data = JSON.parse(event.body);
@@ -43,13 +44,25 @@ const triggerStream = (event, context, cb) => {
 
   const eventData = event.Records[0];
   console.log(eventData.dynamodb.NewImage);
-
+  // Call a function with the newimage that sends it to the relevant phone?
   cb(null, null);
+};
+
+const getUser = (event, context, cb) => {
+  getUserDetails(event.pathParameters.username)
+    .then(result => {
+      const response = {
+        body: JSON.stringify(result)
+      };
+      cb(null, response);
+    })
+    .catch(cb);
 };
 
 module.exports = {
   createMessage,
   getUserMessages,
   getAllMessages,
-  triggerStream
+  triggerStream,
+  getUser
 };
